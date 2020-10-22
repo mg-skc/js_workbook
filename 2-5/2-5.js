8e786352ffaf74750321884edf0d32711af85731
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-10-22 16:25:41 -0500

    2-5 tasks

---
+++ 2-5/2-5.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let name = "Ilya"; 
+
+
+
+//alert( `hello ${"name"}` );
+
+alert( `hello ${name}` ); // ?
+
+
+//alert("${1}"); 
+
+//first showed ${1}
+//next it was hello name
+//final hello Ilya
