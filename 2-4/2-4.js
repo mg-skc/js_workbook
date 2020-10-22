1f1bd951ef54a2093dde25145cb56478678b64fa
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-10-22 12:53:22 -0500

    2.4 tasks

---
+++ 2-4/2-4.js
@@ -0,0 +1,8 @@
+let name="John";
+let admin=name;
+alert(admin)
+let planet="Earth";
+let currentVisitor;
+/*I think that Birthday variable should be capitalized...it is a fixed
+date for that person forever. However, Age should NOT be capitalized
+because age will change over time...the birthday won't.*/
