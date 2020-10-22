e63b3a43d1fc39ac4718a43ee99c5a279b5cd2e8
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-10-22 17:45:17 -0500

    2-6

---
+++ 2-6/2-6.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let name = prompt("What's your name?","");
+
+
+
+
+//alert( `hello ${"name"}` );
+
+alert( `${name}, your name is awesome!` ); // ?
+
+
+//alert("${1}"); 
+
+//first showed ${1}
+//next it was hello name
+//final hello Ilya
