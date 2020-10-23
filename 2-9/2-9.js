7d387d2a60d067548937027f0019c8baa9082e7d
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-10-23 06:49:56 -0500

    2-9

---
+++ 2-9/2-9.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+let a=5 > 4
+//true
+let b = "apple" > "pineapple"
+//false
+let c = "2" > "12"
+//true
+let d = undefined == null
+//true
+let e = undefined === null
+//false
+let f = null == "\n0\n"
+//false
+let g = null === +"\n0\n"
+//false
+
+
+//alert( `hello ${"name"}` );
+
+alert( `a's value is ${a}` ); // ?
+alert( `b's value is ${b}` ); // ?
+alert( `c's value is ${c}` ); // ?
+alert( `d's value is ${d}` ); // ?
+alert( `e's value is ${e}` ); // ?
+alert( `f's value is ${f}` ); // ?
+alert( `g's value is ${g}` ); // ?
+
+//alert("${1}"); 
+
