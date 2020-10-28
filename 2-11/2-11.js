3be119575440d6dd81fc8f7e81bff2146c31352b
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-10-28 12:12:12 -0500

    exercises

---
+++ 2-11/2-11.js
@@ -0,0 +1,113 @@
+"use strict"
+
+//task 1 returnd 2
+//alert(null||2||undefined)
+
+//task 2 returned 1 and 2
+//alert( alert(1)||2||alert(3) );
+
+//task 3 returned null
+//alert( 1 && null && 2 );
+
+//task 4 returned 1 then undef
+//alert( alert(1) && alert(2) );
+
+//task 5 returned 3
+//alert( null||2 && 3||4);
+
+//task 6
+//if age<=14 && age>=90
+
+//task 7
+//if (age !<=14 && age !>=90);
+
+//if ()
+
+//task 8 triggered first, third
+//if (-1 || 0) alert( 'first' );
+//if (-1 && 0) alert( 'second' );
+//if (null || -1 && 1) alert( 'third' );
+
+//task 9
+let whosThere = prompt("Who's there!")
+
+if (whosThere = "Other") {
+  alert("I don't know you")
+} else if (whosThere="Admin"){
+  prompt("Password?")
+};
+
+
+/*let numInput = prompt("Input a number!");
+
+if (numInput>0) {
+    alert("1")
+} else if (numInput<0){
+    alert("-1");
+} else {
+  alert("0");
+}
+
+
+
+
+let answer = prompt("What's the “official” name of JavaScript?");
+
+if (answer == "ECMAScript") {
+    alert("Correct!")
+} else {
+    alert("You don't know? ECMAScript");
+}
+*/
+
+
+
+
+//  answer to task #3    let result = (a + b < 4) ? below:over;
+
+/*let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+*/
+
+/*
+let login = prompt("Role at company?");
+
+let message = (login=="Employee") ? 'Hello' :
+  (login=="Director") ? 'Greetings!' :
+  (login=="") ? 'No login!' :
+  '';
+
+alert( message );
+*/
+
+
+/*
+if (year == 2015) {
+    alert( 'You guessed it right!' );
+  } else {
+    alert( 'How can you be so wrong?' ); // any value except 2015
+  }
+  */
+
+//alert( `hello ${"name"}` );
+/*
+alert( `a's value is ${a}` ); // ?
+alert( `b's value is ${b}` ); // ?
+alert( `c's value is ${c}` ); // ?
+alert( `d's value is ${d}` ); // ?
+alert( `e's value is ${e}` ); // ?
+alert( `f's value is ${f}` ); // ?
+alert( `g's value is ${g}` ); // ?
+*/
+
+//alert("${1}"); 
+
---
+++ 2-15/2-15.js
@@ -0,0 +1,151 @@
+"use strict"
+
+
+//task
+/*function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Did parents allow you?');
+  }
+}
+
+function checkAge(age){
+  return (age>18) ? true|| confirm("Did parents allow you?")
+}
+
+function checkAge(age)  
+return (age>18) || confirm("Did parents allow you?")
+*/
+
+/*
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm('Did parents allow you?');
+  }
+}
+*/
+/*
+function min(a,b) {
+ a=3;
+ b=4;
+  if (a>b){
+    return b;}
+    else {return a;}
+  }
+alert("The result is:" + min());
+*/
+
+let x;
+let n;
+let z;
+
+function pow(x,n) {
+  x= prompt ("what number would you like?");
+  n= prompt("To what power to multiply?");
+  z=(x**n);
+  if (Number.isInteger(z)){
+ alert("The result is:" + (z));
+  } else {alert("Try again");}
+  }
+  if (pow(x,n));
+
+
+
+
+
+
+
+//let age = prompt('How old are you?', 18);
+
+
+
+
+
+/*
+task 9
+let whosThere = prompt("Who's there!")
+
+if (whosThere = "Other") {
+  alert("I don't know you")
+} else if (whosThere="Admin"){
+  prompt("Password?")
+};
+*/
+
+/*let numInput = prompt("Input a number!");
+
+if (numInput>0) {
+    alert("1")
+} else if (numInput<0){
+    alert("-1");
+} else {
+  alert("0");
+}
+
+
+
+
+let answer = prompt("What's the “official” name of JavaScript?");
+
+if (answer == "ECMAScript") {
+    alert("Correct!")
+} else {
+    alert("You don't know? ECMAScript");
+}
+*/
+
+
+
+
+//  answer to task #3    let result = (a + b < 4) ? below:over;
+
+/*let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+*/
+
+/*
+let login = prompt("Role at company?");
+
+let message = (login=="Employee") ? 'Hello' :
+  (login=="Director") ? 'Greetings!' :
+  (login=="") ? 'No login!' :
+  '';
+
+alert( message );
+*/
+
+
+/*
+if (year == 2015) {
+    alert( 'You guessed it right!' );
+  } else {
+    alert( 'How can you be so wrong?' ); // any value except 2015
+  }
+  */
+
+//alert( `hello ${"name"}` );
+/*
+alert( `a's value is ${a}` ); // ?
+alert( `b's value is ${b}` ); // ?
+alert( `c's value is ${c}` ); // ?
+alert( `d's value is ${d}` ); // ?
+alert( `e's value is ${e}` ); // ?
+alert( `f's value is ${f}` ); // ?
+alert( `g's value is ${g}` ); // ?
+*/
+
+//alert("${1}"); 
+
