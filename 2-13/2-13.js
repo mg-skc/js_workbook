03302b722f1fd0bd995c43e4723fe6abddda0728
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-10-30 11:30:13 -0500

    exercises and fizzbuzz

---
+++ 2-13/2-13.js
@@ -0,0 +1,32 @@
+"use strict"
+/*  Task 1 - stopped at 1 b/c it stops at 0
+let i = 3;
+
+while (i) {
+  alert( i-- );
+}
+*/
+
+/*Task 2
+//goes to 4 because the increment happens before eval the iteration
+//let i = 0;
+//while (++i < 5) alert( i );
+
+Goes to 5 because increment happens AFTER the evaluation
+let i = 0;
+while (i++ < 5) alert( i );
+*/
+
+//Task 3 - first will go to 4 next to 5...was my answer...got it wrong. :(
+
+/*Task 4
+for(let i=2;i<=10;i++) {
+  if (i%2==0) {
+alert(i);}}
+*/
+
+/* Task 5
+
+
+
+
