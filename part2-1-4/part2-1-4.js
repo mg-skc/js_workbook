52f7d22eb073d6133941ff3d6773e5a9c77d7dcc
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-10-30 21:04:19 -0500

    part 2 exercises

---
+++ part2-1-4/part2-1-4.js
@@ -0,0 +1,15 @@
+"use strict"
+
+//task 1
+//document.body.firstElementChild
+
+//document.body.lastElementChild
+
+//document.body.lastElementChild.lastElementChild
+
+//Task2
+//Yes, then No
+
+//Task 3
+//
+
